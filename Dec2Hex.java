a29c6df75d40b6f994066e289805d40258c36330
“adamhanno” <ahano@ymail.com>
2023-11-13 07:44:44 +0000

    stil in an other Java Corrected

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,7 +7,7 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        Arg1 = Integer.parseInt(args[]);
+        Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
